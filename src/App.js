e4d427d0547b85c2ddb6b51ef30d378fd8cde674
Cem Berke Cebi <cemberke.cebi@trendyol.com>
2020-11-26 17:01:10 +0300

    edit, delete, update todo functions added

--- src/App.js
+++ src/App.js
@@ -3,28 +3,36 @@ import "./App.css";
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 import Todo from "./Todo";
 import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+
   //this code fires when app.js loads
   useEffect(() => {
-    db.collection("todos").onSnapshot((snapshot) => {
-      console.log(snapshot.docs.map((doc) => doc.data().todo));
-
-      setTodos(snapshot.docs.map((doc) => doc.data().todo));
-    });
+    db.collection("todos")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
+        );
+      });
   }, []);
 
   const addTodo = (event) => {
     event.preventDefault(); // will stop refreshing
-    setTodos([...todos, input]);
+
+    db.collection("todos").add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
     setInput(""); // clear input
   };
 
   return (
     <div className="App">
-      <h1>Hello World! </h1>
+      <h1>Todo List! </h1>
       <form>
         <FormControl>
           <InputLabel>✅ Write a Todo </InputLabel>
@@ -46,7 +54,7 @@ function App() {
       </form>
       <ul>
         {todos.map((todo) => (
-          <Todo text={todo} />
+          <Todo todo={todo} />
         ))}
       </ul>
     </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,20 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Todo.css";
 import {
+  Button,
+  IconButton,
+  Input,
   List,
   ListItem,
   ListItemText,
-  ListItemAvatar,
+  Modal,
 } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import DeleteIcon from "@material-ui/icons/Delete";
+import db from "./firebase";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Todo(props) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const updateTodo = () => {
+    db.collection("todos").doc(props.todo.id).set(
+      {
+        todo: input,
+      },
+      { merge: true }
+    );
+    setOpen(false);
+  };
+
   return (
-    <List className="todo_list">
-      <ListItem>
-        <ListItemAvatar />
-        <ListItemText primary={props.text} secondary="Dummy Deadline" />
-      </ListItem>
-    </List>
+    <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div className={classes.paper}>
+          <h1>Update Your Todo</h1>
+          <Input
+            placeholder={props.todo.todo}
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="secondary"
+            type="submit"
+            onClick={() => updateTodo()}
+          >
+            {" "}
+            Update
+          </Button>
+        </div>
+      </Modal>
+
+      <Grid
+        container
+        spacing={3}
+        justify="center"
+        style={{ paddingTop: "20px" }}
+      >
+        <List className="todo_list">
+          <ListItem>
+            <ListItemText
+              primary={props.todo.todo}
+              secondary="Dummy Deadline 🕰️"
+            />
+          </ListItem>{" "}
+        </List>
+
+        <Grid item>
+          <Button
+            variant="contained"
+            color="secondary"
+            type="submit"
+            size="medium"
+            onClick={(e) => setOpen(true)}
+          >
+            {" "}
+            Edit
+          </Button>
+          <IconButton aria-label="delete" className={classes.margin}>
+            <DeleteIcon
+              fontSize="large"
+              onClick={() => db.collection("todos").doc(props.todo.id).delete()}
+            />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
